40e898bb1460d97efe64da7ecb3daca58e7c4fff
danielsm797 <eduardodaniel797@gmail.com>
2022-02-20 11:59:41 -0500

    Ejercicio 2.6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const add = (event) => {
+
+    event.preventDefault();
+
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={add}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {
+          persons.map(x => <li key={x.name}>{x.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
